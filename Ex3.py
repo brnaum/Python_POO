2b7b7509e30d473a09d7cb72d107aef89df2c894
brnaum <brndojob@gmail.com>
2026-07-21 16:54:52 -0300

    terminando ex3

--- Ex3.py
+++ Ex3.py
@@ -6,7 +6,16 @@
 class Criptografia: 
     def __init__(self, frase):
         self.frase = frase 
-    def criptografar:
-    texto = self.frase
-    texto = texto.replace("A", "4").replace("a", "4")
-    texto = texto.replace("E", "3").replace("e", "3")
+    def criptografar(self):
+        texto = self.frase
+        texto = texto.replace("A", "4").replace("a", "4")
+        texto = texto.replace("E", "3").replace("e", "3")
+        texto = texto.replace("I", "1").replace("i", "1")
+        texto = texto.replace("O", "0").replace("o", "0")
+        texto = texto.replace("U", "8").replace("u", "8")
+        return texto
+
+texto = input("Digite uma frase: ")
+cripto = Criptografia(texto)
+print("Frase Criptografada:")
+print(cripto.criptografar())
